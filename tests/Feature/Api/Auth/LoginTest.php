23a0cc3e8f7d27b0f4a2016c1d9dd48d4ec164cf
Conan Torres <conantorreswf@gmail.com>
2024-07-29 17:52:13 -0300

    testa requires

--- tests/Feature/Api/Auth/LoginTest.php
+++ tests/Feature/Api/Auth/LoginTest.php
@@ -40,3 +40,48 @@ test('should fail auth - wrong email', function () {
     postJson(route('auth.login'), $data)
         ->assertStatus(422);
 });
+
+describe('validations', function () {
+    it('should require email', function () {
+        $data = [
+            'password' => 'password',
+            'device_name' => 'e2etest',
+        ];
+
+        PostJson(route('auth.login'), $data)
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'email' => trans('validation.required', ['attribute' => 'email']),
+            ]);
+    });
+
+    it('should require password', function () {
+
+        $user = User::factory()->create();
+        $data = [
+            'email' => $user->email,
+            'device_name' => 'e2etest',
+        ];
+
+        PostJson(route('auth.login'), $data)
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'password' => trans('validation.required', ['attribute' => 'password']),
+            ]);
+    });
+
+    it('should require device_name', function () {
+
+        $user = User::factory()->create();
+        $data = [
+            'email' => $user->email,
+            'password' => 'password',
+        ];
+
+        PostJson(route('auth.login'), $data)
+            ->asserstatus(422)
+            ->assertJsonValidationErrors([
+                'device_name' => trans('validation.required', ['attribute' => 'device name']),
+            ]);
+    });
+});
